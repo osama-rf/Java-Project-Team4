04209f75c3e090ddfb25360c1c3561f9a6b6dcfe
osama-rf <osama.refae12901@icloud.com>
2022-08-07 14:31:44 +0300

    inital commit

---
+++ src/main/java/com/group4/mim/MiMApplication.java
@@ -0,0 +1,13 @@
+package com.group4.mim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiMApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiMApplication.class, args);
+    }
+
+}
